859c3adf654aa1e524ab93f98f62af211273ab59
AfifZuhair08 <afifzuhair08@gmail.com>
2021-05-29 23:52:03 +0800

    Developer Fundamentals 1

---
+++ devsOne.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python3
+
+# operator precedence
+# -->>
+# (), **, * /, + -
+print((20 - 3) * 4 ** 2)
+
+# returns binary
+print(bin(5))
+
+# convert bin to num
+print(int('0b101',2))
+
+# augmented assignment operator
+a = 5
+a +=2
+print(a)
+
+# long string
+strings = '''
+dwfaqefqe
+qwfqefqef
+qfqwergweg
+'''
+print(strings)
+
+# concatenation
+fname = 'Afif'
+lname = 'Zuhair'
+print (fullname = fname + lname)
--- tes.py
+++ tes.py
@@ -1,8 +1,20 @@
 # !/usr/bin/env python3
+
+# basic
 print("amigo")
 print(4-2)
 print(4+2)
 print(4*2)
 print(4/2)
 
+# typee of
 print(type(4/2))
+
+# special operator
+print(4**2)
+print(4//2)
+print(4 % 2)
+
+# math funtions
+print(round(4.6))
+print(abs(-20))
